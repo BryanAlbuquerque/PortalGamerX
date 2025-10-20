7b3f57c40fabe212414aa5ffc4e2f96623938f9b
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-10-20 12:32:31 -0300

    Ajustando o MVC

--- PortalGamerX/obj/Debug/net9.0/PortalGamerX.AssemblyInfo.cs
+++ PortalGamerX/obj/Debug/net9.0/PortalGamerX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalGamerX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b1187f7cf65bd249787e8121c12931407f24375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73526f7e31475d186a1c048c64fc4dab99cf4ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalGamerX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalGamerX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
