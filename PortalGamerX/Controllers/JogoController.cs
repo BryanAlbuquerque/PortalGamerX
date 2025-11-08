c359d7daf7ffed52af4a209b19c5767b72771f84
Bryan Albuquerque <143979646+BryanAlbuquerque@users.noreply.github.com>
2025-11-08 10:39:52 -0300

    Views Jogos e produtos

---
+++ PortalGamerX/Controllers/JogoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalGamerX.Repository.Interfaces;
+
+namespace PortalGamerX.Controllers
+{
+    public class JogoController : Controller
+    {
+        private readonly IJogoRepository _jogoRepository;
+
+        public JogoController(IJogoRepository jogoRepository)
+        {
+            _jogoRepository = jogoRepository;
+        }
+
+        public IActionResult List()
+        {
+            var jogos = _jogoRepository.Jogos;
+            return View(jogos);
+        }
+    }
+}
---
+++ PortalGamerX/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortalGamerX.Repository.Interfaces;
+
+namespace PortalGamerX.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository; 
+        public ProdutoController(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public IActionResult List()
+        {
+            var produtos = _produtoRepository.Produtos;
+            return View(produtos);
+        }
+    }
+}
