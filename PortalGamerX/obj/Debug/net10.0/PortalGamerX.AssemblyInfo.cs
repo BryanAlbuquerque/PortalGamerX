f7ac53db974280d7034d59cd4cdc2e28c2b5d665
BryanAlbuquerque <bryanalburquerque2@gmail.com>
2026-02-14 17:35:20 -0300

    Atualizando paginas Jogos e produtos

--- PortalGamerX/obj/Debug/net10.0/PortalGamerX.AssemblyInfo.cs
+++ PortalGamerX/obj/Debug/net10.0/PortalGamerX.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalGamerX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f40ac541679625a535196776d9deef8cc60ac68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58fadcbbac799dc4ff599339dac3ab63eb6052d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalGamerX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalGamerX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
